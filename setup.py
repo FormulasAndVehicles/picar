83ea552edc6b060b6f7ef8b33c46df1a28e9c610
Thies Lennart Alff <lennart.alff@gmail.com>
2021-09-29 15:49:05 +0000

    fixed setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['common'],
-    package_dir={'': 'dir'},
+    packages=['picar'],
+    package_dir={'': 'src'},
 )
 
 setup(**setup_args)
